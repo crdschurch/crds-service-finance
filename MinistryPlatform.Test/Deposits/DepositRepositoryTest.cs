1cf02439b3d35cb9f06c94c7458407f794480b78
jpc11623 <john.cleaver@ingagepartners.com>
2017-12-01 14:54:48 -0500

    US10966 - Create Deposits in MP
    Adding some testing

---
+++ MinistryPlatform.Test/Deposits/DepositRepositoryTest.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Crossroads.Web.Common.Configuration;
+using Crossroads.Web.Common.MinistryPlatform;
+using MinistryPlatform.Deposits;
+using MinistryPlatform.Models;
+using Moq;
+using Xunit;
+
+namespace MinistryPlatform.Test.Deposits
+{
+    public class DepositRepositoryTest
+    {
+        readonly Mock<IApiUserRepository> _apiUserRepository;
+        readonly Mock<IConfigurationWrapper> _configurationWrapper;
+        readonly Mock<IMinistryPlatformRestRequestBuilder> _restRequest;
+        readonly Mock<IMinistryPlatformRestRequestBuilderFactory> _restRequestBuilder;
+        readonly Mock<IMinistryPlatformRestRequest> _request;
+        readonly Mock<IMapper> _mapper;
+
+        private readonly DepositRepository _fixture;
+
+        private string token = "123abc";
+
+        public DepositRepositoryTest()
+        {
+            _apiUserRepository = new Mock<IApiUserRepository>(MockBehavior.Strict);
+            _restRequestBuilder = new Mock<IMinistryPlatformRestRequestBuilderFactory>(MockBehavior.Strict);
+            _configurationWrapper = new Mock<IConfigurationWrapper>(MockBehavior.Strict);
+            _restRequest = new Mock<IMinistryPlatformRestRequestBuilder>(MockBehavior.Strict);
+            _mapper = new Mock<IMapper>(MockBehavior.Strict);
+            _request = new Mock<IMinistryPlatformRestRequest>();
+
+            _apiUserRepository.Setup(r => r.GetDefaultApiUserToken()).Returns(token);
+            _restRequestBuilder.Setup(m => m.NewRequestBuilder()).Returns(_restRequest.Object);
+            _restRequest.Setup(m => m.WithAuthenticationToken(token)).Returns(_restRequest.Object);
+            _restRequest.Setup(m => m.Build()).Returns(_request.Object);
+
+            _fixture = new DepositRepository(_restRequestBuilder.Object,
+                _apiUserRepository.Object,
+                _configurationWrapper.Object,
+                _mapper.Object);
+        }
+
+        [Fact]
+        public void ShouldCreateDeposit()
+        {
+            // Arrange
+            var processorTransferId = "1a2b3c";
+
+            var mpDeposit = new MpDeposit
+            {
+                ProcessorTransferId = processorTransferId
+            };
+
+            var newMpDeposit = new MpDeposit
+            {
+                Id = 123,
+                ProcessorTransferId = processorTransferId
+            };
+
+            _request.Setup(m => m.Create(It.IsAny<MpDeposit>())).Returns(newMpDeposit);
+
+            // Act
+            var result = _fixture.CreateDeposit(mpDeposit);
+
+            // Assert
+            Assert.Equal(processorTransferId, result.ProcessorTransferId);
+        }
+    }
+}
---
+++ MinistryPlatform.Test/Donations/DonationRepositoryTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinistryPlatform.Test.Donations
+{
+    public class DonationRepositoryTest
+    {
+    }
+}
