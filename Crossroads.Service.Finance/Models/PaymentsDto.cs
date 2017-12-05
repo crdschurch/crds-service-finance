4aa060a856917dca8639fb134acf9be4242797ae
jpc11623 <john.cleaver@ingagepartners.com>
2017-12-05 11:27:28 -0500

    US10966 - Create Deposits in MP
    Updated to handle the page size stuff

--- Crossroads.Service.Finance/Models/PaymentsDto.cs
+++ Crossroads.Service.Finance/Models/PaymentsDto.cs
@@ -8,6 +8,16 @@ namespace Crossroads.Service.Finance.Models
 {
     public class PaymentsDto
     {
+        [JsonProperty("page")]
+        public int Page { get; set; }
+
+        // currently, Pushpay has this hard set to 25, but this could change
+        [JsonProperty("pageSize")]
+        public int PageSize { get; set; }
+
+        [JsonProperty("totalPages")]
+        public int TotalPages { get; set; }
+
         // TODO: Check the property name and type needed here
         [JsonProperty("items")]
         public List<PaymentProcessorChargeDto> payments { get; set; }
