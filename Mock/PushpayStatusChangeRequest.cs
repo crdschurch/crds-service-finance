a450d0f47d7044199d6dac3a4019d5ea6a68048e
Tim Giblin <timmyg13@gmail.com>
2017-12-11 16:57:24 -0500

    add webhook request

---
+++ Mock/PushpayStatusChangeRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Crossroads.Service.Finance.Models;
+
+namespace Mock
+{
+    public class PushpayStatusChangeRequest
+    {
+        public static PushpayWebhook Create() =>
+            new PushpayWebhook()
+            {
+                Subscription = "https://sandbox-api.pushpay.io/v1/merchant/NzkwMjY0NTpuSzZwaUgzakc4WHdZVy1xd0ZVNnlzTlF2aTg/webhook/8DWULX3yULJimB3oqaYj8w",
+                Events = new List<PushpayWebhookEvent>
+                {
+                new PushpayWebhookEvent
+                    {
+                        Date = DateTime.Parse("2017-12-01T15:55:51Z"),
+                        EventType = "payment_status_changed",
+                        EntityType = "Payment",
+                        From = "processing",
+                        To = "success",
+                        Links = new PushpayWebhookLinks() {
+                            Payment = "https://sandbox-api.pushpay.io/v1/merchant/NzkwMjY0NTpuSzZwaUgzakc4WHdZVy1xd0ZVNnlzTlF2aTg/payment/5dcdaPH0wkqvnd4LmB7dEg",
+                            Merchant = "https://sandbox-api.pushpay.io/v1/merchant/NzkwMjY0NTpuSzZwaUgzakc4WHdZVy1xd0ZVNnlzTlF2aTg"
+                        }
+                    }
+                }
+            };
+    }
+}
