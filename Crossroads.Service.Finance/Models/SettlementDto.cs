fe8fd5a4c42a797e6221f84cbb9b1fe2fddc3a9e
jpc11623 <john.cleaver@ingagepartners.com>
2017-12-15 15:38:57 -0500

    US10971 - Trigger Creating Deposits
    Fixing date time issue and mapping issue

--- Crossroads.Service.Finance/Models/SettlementDto.cs
+++ Crossroads.Service.Finance/Models/SettlementDto.cs
@@ -14,7 +14,19 @@ namespace Crossroads.Service.Finance.Models
         [JsonProperty("name")]
         public string Name { get; set; }
 
+        [JsonProperty("totalAmount")]
+        public AmountDto TotalAmount { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("totalPayments")]
+        public int TotalPayments { get; set; }
+
         [JsonProperty("estimatedDepositDate")]
         public DateTime EstimatedDepositDate { get; set; }
+
+        [JsonProperty("isReconciled")]
+        public bool IsReconciled { get; set; }
     }
 }
