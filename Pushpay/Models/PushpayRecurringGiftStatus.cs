d0ad519b1af4fc7f624ecd16bba3a0d44a1eee20
CSSB0009\bcavanaugh <cavanaughbc@gmail.com>
2018-07-12 16:01:50 -0400

    Updates for recurring gift status updates from push pay

---
+++ Pushpay/Models/PushpayRecurringGiftStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Pushpay.Models
+{
+    public static class PushpayRecurringGiftStatus
+    {
+        public const string Active = "Active";
+        public const string Paused = "Paused";
+        public const string Cancelled = "Cancelled";
+    }
+}
