be786bd1cd4b56e32a9c56aa3b1e618dc2bc3ae2
candrews-thrive <chris.andrews@thrivecincinnati.com>
2018-09-08 23:43:29 -0400

    fixed test

--- MinistryPlatform.Test/Pledges/PledgeRepositoryTest.cs
+++ MinistryPlatform.Test/Pledges/PledgeRepositoryTest.cs
@@ -67,6 +67,7 @@ namespace MinistryPlatform.Test.Pledges
             };
             var filter = $"Pledge_Status_ID_Table.[Pledge_Status_ID] IN (1,2)";
             filter += $" AND Donor_ID_Table_Contact_ID_Table.[Contact_ID] = {contactId}";
+            filter += $" AND Pledge_Campaign_ID_Table_Pledge_Campaign_Type_ID_Table.[Pledge_Campaign_Type_ID] = 1";
             _apiUserRepository.Setup(r => r.GetApiClientToken("CRDS.Service.Finance")).Returns(token);
             _restRequestBuilder.Setup(m => m.NewRequestBuilder()).Returns(_restRequest.Object);
             _restRequest.Setup(m => m.WithAuthenticationToken(token)).Returns(_restRequest.Object);
