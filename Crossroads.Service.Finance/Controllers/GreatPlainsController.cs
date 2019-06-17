0057d458052c8375c9e798d72b0c4ebb5fc729e0
vchtel <VictorChtelmakh@gmail.com>
2019-06-17 14:00:37 -0400

    Add a new endpoint which will be called to adjust journal entries.

---
+++ Crossroads.Service.Finance/Controllers/GreatPlainsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crossroads.Service.Finance.Controllers
+{
+  [Produces("application/json")]
+  [Route("api/GreatPlains")]
+  public class GreatPlainsController : Controller
+  {
+    [HttpGet]
+    [Route("adjustjournalentries")]
+    public IActionResult AdjustJournalEntries()
+    {
+      return Ok();
+    }
+  }
+}
