d75b5c28b7ea5d4df34aae6820256aa7cebee01b
vchtel <VictorChtelmakh@gmail.com>
2019-10-25 14:20:17 -0400

    More of the same.

---
+++ Crossroads.Service.Finance/Services/JournalEntry/IJournalEntryService.cs
@@ -0,0 +1,14 @@
+﻿using Exports.Models;
+using MinistryPlatform.Models;
+using System.Collections.Generic;
+
+namespace Crossroads.Service.Finance.Services.JournalEntry
+{
+    public interface IJournalEntryService
+    {
+        MpJournalEntry AdjustExistingJournalEntry(MpDistributionAdjustment mpDistributionAdjustment, MpJournalEntry journalEntry);
+        MpJournalEntry NetCreditsAndDebits(MpJournalEntry journalEntry);
+        MpJournalEntry CreateNewJournalEntry(string batchId, MpDistributionAdjustment mpDistributionAdjustment);
+        List<MpJournalEntry> RemoveWashEntries(List<MpJournalEntry> journalEntries);
+    }
+}
