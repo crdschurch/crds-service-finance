5a0bffc3cee9931fbbb0c59196c10a616f66cb52
Chris Andrews <22330518+candrews55@users.noreply.github.com>
2020-08-12 17:35:18 -0400

    Refactor to support to functions and share code

--- Crossroads.Functions.Finance/SyncLog.cs
+++ Crossroads.Functions.Finance/SyncLog.cs
@@ -3,15 +3,15 @@ using Microsoft.Azure.Cosmos.Table;
 
 namespace Crossroads.Functions.Finance
 {
-    public class DonationSyncLog : TableEntity
+    public class SyncLog : TableEntity
     {
         public string LogType { get { return this.PartitionKey; } }
         public string LogKey { get { return this.RowKey; } }
         public DateTime LogTimestamp { get; set; }
         public string LogStatus { get; set; }
 
-        public DonationSyncLog() { }
-        public DonationSyncLog(DateTime logTimestamp, string logStatus)
+        public SyncLog() { }
+        public SyncLog(DateTime logTimestamp, string logStatus)
         {
             this.PartitionKey = "DonationSyncLog";
             this.RowKey = Guid.NewGuid().ToString();
