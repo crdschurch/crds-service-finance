67cbea7f6cac66e803fc3f0b983e0a3894892477
Chris Andrews <22330518+candrews55@users.noreply.github.com>
2020-08-13 11:37:11 -0400

    Use builtin helpers for query string

--- Crossroads.Functions.Finance/RecurringScheduleSync.cs
+++ Crossroads.Functions.Finance/RecurringScheduleSync.cs
@@ -1,10 +1,10 @@
-﻿using Microsoft.Azure.WebJobs;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Net;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
 
 namespace Crossroads.Functions.Finance
 {
@@ -35,15 +35,16 @@ namespace Crossroads.Functions.Finance
         private static async Task<HttpStatusCode> RunRecurringScheduleEndpointAsync(string lastSuccessfulRunTime,
             string currentTime, ILogger log)
         {
-            string endpointUrl = Environment.GetEnvironmentVariable("ENDPOINT_URL") +
-                                 $"Pushpay/updaterecurringgifts?startDate={lastSuccessfulRunTime}&endDate={currentTime}";
+            var endpointUrl = $"{Environment.GetEnvironmentVariable("ENDPOINT_URL")}Pushpay/updaterecurringgifts";
+            var urlBuilder = new UriBuilder(endpointUrl);
+            var query = HttpUtility.ParseQueryString(urlBuilder.Query);
+            query["startDate"] = lastSuccessfulRunTime;
+            query["endDate"] = currentTime;
+            urlBuilder.Query = query.ToString();
+            
             HttpClient httpClient = new HttpClient();
-            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, endpointUrl);
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, urlBuilder.Uri);
 
-            httpRequestMessage.Content = new StringContent(
-                "{\"lastSuccessfulRunTime\":\"" + lastSuccessfulRunTime + "\"}",
-                Encoding.UTF8,
-                "application/json");
             log.LogInformation($"Endpoint URL: {endpointUrl}");
             HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
             return httpResponseMessage.StatusCode;
