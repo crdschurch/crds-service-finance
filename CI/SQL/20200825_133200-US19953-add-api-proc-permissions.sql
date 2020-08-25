d8c54de4198e57e7fca47ea434d9352580fb9880
Chris Andrews <22330518+candrews55@users.noreply.github.com>
2020-08-25 14:31:29 -0400

    Create stored proc and create permissions for it

---
+++ CI/SQL/20200825_133200-US19953-add-api-proc-permissions.sql
@@ -0,0 +1,26 @@
+-- ===============================================================
+-- Authors: Chris Andrews
+-- Create date: 2020-08-25
+-- Description:	Add API client permissions
+-- ===============================================================
+
+IF NOT EXISTS(SELECT * FROM [dbo].[dp_API_Procedures] WHERE [procedure_name] = 'api_crds_Insert_PushpayRecurringSchedulesRawJson')
+BEGIN
+	DECLARE @IDs TABLE (ID INT)
+
+	INSERT INTO [dbo].[dp_API_Procedures] ([procedure_name]) 
+	OUTPUT INSERTED.API_Procedure_ID INTO @IDs
+	VALUES('api_crds_Insert_PushpayRecurringSchedulesRawJson')
+
+	IF NOT EXISTS(select * from [dbo].[dp_Role_API_Procedures] WHERE API_Procedure_ID = (SELECT TOP(1) * FROM @IDs) AND Role_ID = 1023)
+	BEGIN
+		INSERT INTO [dbo].[dp_Role_API_Procedures]
+		([Role_ID],
+		[API_Procedure_ID],
+		[Domain_ID])
+		VALUES
+		(1023,
+		(SELECT TOP(1) * FROM @IDs),
+		1)
+	END
+END
